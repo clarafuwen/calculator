148840257f88f8a248bf8687ff2a7123c1d850e1
clarafuwen <clarafuwen@gmail.com>
2024-10-22 14:44:59 -0400

    feat: add support for backspace and consecutive input
    add backspace to delete digits, decimal point and operator
    allow consecutive input in the format of "1 + 7 * 7 - 4.5..."

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const decimal = digits.querySelector(".decimal");
 const operators = document.querySelector(".operators");
 const equals = document.querySelector(".equals");
 const ac = document.querySelector(".ac");
+const back = document.querySelector(".delete");
 const display = document.querySelector(".display");
+let input = [];
 let op1_array = [];
 let op2_array = [];
 let operand1 = null;
@@ -19,25 +21,43 @@ digits.addEventListener("click", (e)=>{
         operand1? op2_array.push(e.target.textContent): op1_array.push(e.target.textContent);
     }
     display.textContent = operand1? op2_array.join("") : op1_array.join("");
+    input.push(e.target.textContent);
 })
 
 operators.addEventListener("click", (e) =>{
     if(!operand1){
         operand1 = Number(op1_array.join(""));
         decimal.classList.remove("on");
+    } 
+    if(!operand2){
+        operand2 = Number(op2_array.join(""));
+        decimal.classList.remove("on");
     }
+    //consecutive input in the format of (4+6-9...) operand1 operator operand2 operator operand2 ..., 
+    //treat the second operator as equals and stores the operator for later calculation
+    //e.g. 4 + 6 - 9 becomes 4 + 6 = 10, then 10 - 9  = 1
+    if(operand1.toString() && operator && operand2.toString()){
+        operate(operand1, operator, operand2);
+        display.textContent = result;
+        operand1 = result;
+        op1_array = [];
+        op2_array = [];
+        operand2 = null;
+        operator = e.target.textContent;
+    }
+    
     console.log(operand1);
     operator = e.target.textContent;
-    display.textContent = operator;
+    input.push(e.target.textContent);
+    // display.textContent = operator;
 })
 
 
 equals.addEventListener("click", (e) => {
     if(!operand2){
         operand2 = Number(op2_array.join(""));
+        decimal.classList.remove("on");
     }
-    display.textContent = operand2;
-    decimal.classList.remove("on");
     if(operand1.toString() && operator && operand2.toString()){
         operate(operand1, operator, operand2);
     }
@@ -47,10 +67,24 @@ equals.addEventListener("click", (e) => {
     op2_array = [];
     operand1 = result;
     operand2 = null;
+    operator = null;
 })
 
-ac.addEventListener("click", ()=>{
+ac.addEventListener("click", (e)=>{
+    console.log(e.lastModified);
     clear();
+})
+
+back.addEventListener("click", ()=>{
+    const lastInput = input.pop();
+    if(["+","-","*","/"].includes(lastInput)){
+        operator = null;
+    } else if(!operand1){
+        op1_array.pop();
+    } else if(!operand2){
+        op2_array.pop();
+    } 
+    display.textContent = operand1? operand1 : operand2;
 })
 
 function operate(operand1, operator, operand2){
