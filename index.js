525b55fa202fefef5ecdecea242a8f807ab06cec
clarafuwen <clarafuwen@gmail.com>
2024-10-23 13:07:10 -0400

    feat: add keyboard support support digits, decimal point input from keyboard support backspace button to delete input one at a time from keyboard support clear button to rest from keyboard

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const equals = document.querySelector(".equals");
 const ac = document.querySelector(".ac");
 const back = document.querySelector(".delete");
 const display = document.querySelector(".display");
+const SUPPORTED_KEYBOARD_INPUT = ["1","2","3","4","5","6","7","8","9","0",".","+","-","*","/","=","Clear","Backspace"];
+const OPERATORS = ["+","-","*","/"];
 let input = [];
 let op1_array = [];
 let op2_array = [];
@@ -16,12 +18,61 @@ let result = null;
 digits.addEventListener("click", (e)=>{
     if(e.target.textContent === "." && !decimal.className.includes("on")){
         decimal.classList.add("on");
-        operand1? op2_array.push(e.target.textContent): op1_array.push(e.target.textContent);
+        operand1 !==null? op2_array.push(e.target.textContent): op1_array.push(e.target.textContent);
     } else if(e.target.textContent !== "." ){
-        operand1? op2_array.push(e.target.textContent): op1_array.push(e.target.textContent);
+        operand1 !==null? op2_array.push(e.target.textContent): op1_array.push(e.target.textContent);
     }
-    display.textContent = operand1? op2_array.join("") : op1_array.join("");
+    display.textContent = operand1 !==null? op2_array.join("") : op1_array.join("");
     input.push(e.target.textContent);
+})
+
+document.addEventListener("keydown", (e)=>{
+    console.log(e);
+    if(SUPPORTED_KEYBOARD_INPUT.includes(e.key)){
+        if(e.key === "." && !decimal.className.includes("on")){
+            decimal.classList.add("on");
+            operand1 !== null? op2_array.push(e.key): op1_array.push(e.key);
+            input.push(e.key);
+        } else if(e.key !== "." ){
+            if(OPERATORS.includes(e.key) || e.key==="="){
+                if(operator || e.key === "="){
+                    operand2 = Number(op2_array.join(""));
+                    decimal.classList.remove("on");
+                    operate(operand1, operator, operand2);
+                    operand1 = result;
+                    op1_array = [];
+                    op2_array = [];
+                    operand2 = null;
+                    display.textContent = operand1;
+                } else {
+                    operand1 = operand1 !==null?operand1:Number(op1_array.join(""));
+                    decimal.classList.remove("on");
+                    // display.textContent = operand1? op2_array.join("") : op1_array.join("");
+                }
+                if(e.key !== "="){
+                    operator = e.key;
+                    input.push(e.key);
+                }
+            } else if(e.key==="Backspace"){
+                const lastInput = input.pop();
+                if(["+","-","*","/"].includes(lastInput)){
+                    operator = null;
+                } else if(!operand1){
+                    op1_array.pop();
+                } else if(!operand2){
+                    op2_array.pop();
+                }
+                display.textContent = operand1 !==null? op2_array.join("") : op1_array.join("");
+            } else if(e.key === "Clear"){
+                clear();
+            } else{
+                operand1 !== null? op2_array.push(e.key): op1_array.push(e.key);
+                display.textContent = operand1 !==null? op2_array.join("") : op1_array.join("");
+                input.push(e.key); 
+            }
+        }        
+    }
+
 })
 
 operators.addEventListener("click", (e) =>{
@@ -36,7 +87,7 @@ operators.addEventListener("click", (e) =>{
     //consecutive input in the format of (4+6-9...) operand1 operator operand2 operator operand2 ..., 
     //treat the second operator as equals and stores the operator for later calculation
     //e.g. 4 + 6 - 9 becomes 4 + 6 = 10, then 10 - 9  = 1
-    if(operand1.toString() && operator && operand2.toString()){
+    if(operand1 !==null && operator && operand2 !==null){
         operate(operand1, operator, operand2);
         display.textContent = result;
         operand1 = result;
@@ -58,7 +109,7 @@ equals.addEventListener("click", (e) => {
         operand2 = Number(op2_array.join(""));
         decimal.classList.remove("on");
     }
-    if(operand1.toString() && operator && operand2.toString()){
+    if(operand1 !== null && operator && operand2 !== null){
         operate(operand1, operator, operand2);
     }
     display.textContent = result;
@@ -71,7 +122,6 @@ equals.addEventListener("click", (e) => {
 })
 
 ac.addEventListener("click", (e)=>{
-    console.log(e.lastModified);
     clear();
 })
 
@@ -84,7 +134,7 @@ back.addEventListener("click", ()=>{
     } else if(!operand2){
         op2_array.pop();
     } 
-    display.textContent = operand1? operand1 : operand2;
+    display.textContent = operand1 !==null? op2_array.join("") : op1_array.join("");
 })
 
 function operate(operand1, operator, operand2){
